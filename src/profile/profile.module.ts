1a9288f5088c59985a30b47f86942101f55c25a8
TayeAnimaw <tayeanimaw7@gmail.com>
2026-02-25 16:46:37 +0300

    relationships are made for the  post and the user

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ProfileController } from './profile.controller';
+import { ProfileController, PasswordResetController } from './profile.controller';
 import { ProfileService } from './profile.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [ProfileController],
+  controllers: [ProfileController, PasswordResetController],
   providers: [ProfileService],
   exports: [ProfileService],
 })
